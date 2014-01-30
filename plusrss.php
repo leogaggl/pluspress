b0957760135aa66e8dec65eb8f762942e2d4c096
Leo Gaggl <leo@brightcookie.com.au>
2014-01-31 09:43:54 +1030

    Changes to fix issues with illegal & special characters breaking feed.
    Feed now validated with W3C Feed Validator:
    http://validator.w3.org/feed/

--- plusrss.php
+++ plusrss.php
@@ -4,17 +4,19 @@
 ///////////////////////////////
 
   // Set API key. Get your key at https://code.google.com/apis/console.
-  $key = getenv('PLUS_KEY') ? getenv('PLUS_KEY') : 'insert-key-here';
+  $key = getenv('PLUS_KEY') ? getenv('PLUS_KEY') : 'AIzaSyCk2i4oXtPuRIBzwwkBhnMG6fsoIO0Bm9Y';
 
   // Set ID of Plus user. That's the long number in their profile URL.
-  $uid = getenv('PLUS_ID') ? getenv('PLUS_ID') : '106413090159067280619';
+  $uid = getenv('PLUS_ID') ? getenv('PLUS_ID') : '101636881032878340378';
 
   // Other parameters you can tweak if you like
   $size = 20; // number of RSS items
   $cachetime = 5 * 60;
   $cachefolder = getenv('PLUS_CACHE') ? getenv('PLUS_CACHE') : '/tmp';
-  $cachefile = "$cachefolder/index-cached-".md5($_SERVER["REQUEST_URI"]).".html";
+  $cachefile = "$cachefolder/index-cached-".md5(@$_SERVER['REQUEST_URI']).".html";
   date_default_timezone_set('GMT');
+  
+  header('Content-Type: application/atom+xml');
 
 ///////////////////////////////
 // SERVE FROM CACHE IF EXISTS
@@ -60,22 +62,25 @@
     $content = '';
 
     if ($item->verb == 'share') {
-      $source = "<a href={$object->actor->url}>{$object->actor->displayName}</a>";
-      $content .= "{$item->annotation}<p>&nbsp;<br/><em>$source:</em></p><blockquote>{$object->content}</blockquote>";
-    } else {
-      $content .= $object -> content;
+      $source = "<a href='".$object->actor->url."'>".$object->actor->displayName."</a>";
+      $content .= $item->annotation ."<p>&nbsp;<br/><em>".$source.":</em></p><blockquote>".$object->content."</blockquote>";
+    } 
+    elseif ($item->verb == 'note') {
+    	$content .= $item->title;
+    }
+    else {
+      $content .= iconv("UTF-8","UTF-8//IGNORE", $object->content);
     }
 
     if ($object->attachments and sizeof($object->attachments)) {
       $attachment = $object->attachments[0];
       if ($attachment->objectType == 'photo')
-        $content.="<p><a href='{$attachment->url}'><img width='{$attachment->image->width}' ".
-                  "height='{$attachment->image->height}' src='{$attachment->image->url}' /></a></p>";
+        $content.="<p><a href='".$attachment->url."'><img width='".$attachment->image->width."'height='".$attachment->image->height ."' src='".$attachment->image->url."\ /></a></p>";
       else if ($attachment->objectType='article')
-        $content .= "<p><a href='{$attachment->url}'>{$attachment->displayName}</a></p>";
+        $content .= "<p><a href='".$attachment->url."'>".$attachment->displayName."</a></p>";
     }
 
-    return utf8_encode(htmlspecialchars($content));
+    return html_entity_decode(strip_tags($content));
 
   }
 
@@ -84,22 +89,22 @@
 //////////////////////
 ?>
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'."\n" ?>
-<rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
   <channel>
-    <title><?php= $activities -> title ?></title>
-    <link>http://plus.google.com/<?php= $uid ?>/posts</link>
-    <pubDate><?php= sizeof($items) ? pubDate($items[0]) : ""?></pubDate>
-    <dc:date><?php= sizeof($items) ? $items[0]->published : ""?></dc:date>
+    <atom:link href="https://gaggl.com/plusrss.php" rel="self" type="application/rss+xml" />
+    <title><?php echo $activities -> title ?></title>
+    <link>http://plus.google.com/<?php echo $uid ?>/posts</link>
+    <description>Google+ Public Stream - Leo Gaggl</description>
+    <dc:date><?php echo sizeof($items) ? $items[0]->published : ""?></dc:date>
 <?php foreach ($items as $item) {
    $item_content = content($item);
  ?>
     <item>
-      <title><?php= $item -> title ?>...</title>
-      <link><?php= $item -> url ?></link>
-      <description><?php= $item_content ?></description>
-      <pubDate><?php= pubDate($item) ?></pubDate>
-      <guid><?php= $item -> url ?></guid>
-      <dc:date><?php= $item -> published ?></dc:date>
+      <title><?php echo $item -> title ?>...</title>
+      <link><?php echo $item -> url ?></link>
+      <description><?php echo $item_content ?></description>
+      <guid><?php echo $item -> url ?></guid>
+      <dc:date><?php echo $item -> published ?></dc:date>
     </item>
 <?php } ?>
   </channel>
